b352d84666bb7557c80c8a07d11643a5baa3367b
amcosano <antoniocossano@gmail.com>
2022-04-27 18:45:12 +0200

    implement new feedbacks to fetch on delay

--- src/Spinner/index.js
+++ src/Spinner/index.js
@@ -5,6 +5,11 @@ export default function Spinner() {
   return (
     <>
       <div className='spinner-container'>
+        <div className='spinner-message'>
+          <p>
+            MAY THE <span className='spinner-span'>fetch()</span> BE WITH YOU
+          </p>
+        </div>
         <div className='spinner'></div>
       </div>
     </>
